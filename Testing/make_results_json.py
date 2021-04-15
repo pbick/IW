4bf1370f577393d33675602c03bd3d8a3273127c
pbick <39545188+pbick@users.noreply.github.com>
2021-04-15 14:39:50 -0400

    Update make_results_json.py

--- Testing/make_results_json.py
+++ Testing/make_results_json.py
@@ -21,7 +21,7 @@ def make_results_json(orig_dir, exp_name):
             }
             data[f"{species}/{im}"] = im_dict
     
-    with open(f"Testing/Results/{exp_name}/results.json", 'w') as outfile:
+    with open(f"~/IW/Testing/Results/{exp_name}/results.json", 'w') as outfile:
         json.dump(data, outfile, indent=1)
 
 if __name__ == "__main__":
